8ffa5aa9bba1184073491ffc93773a60ca38be32
JoseMBetancourt <josebetancourt@unicauca.edu.co>
2021-04-14 14:44:16 -0500

    Pagina Principal xml sin funcionalidades

--- app/src/main/java/edu/unicauca/rapirecetas/MainActivity.kt
+++ app/src/main/java/edu/unicauca/rapirecetas/MainActivity.kt
@@ -2,10 +2,19 @@ package edu.unicauca.rapirecetas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun corazonToggle(view: View) {Toast.makeText(this,"Añadido",Toast.LENGTH_SHORT)}
+    fun corazonToggle2(view: View) {Toast.makeText(this,"Añadido",Toast.LENGTH_SHORT)}
+    fun corazonToggle3(view: View) {Toast.makeText(this,"Añadido",Toast.LENGTH_SHORT)}
+    fun corazonToggle4(view: View) {Toast.makeText(this,"Añadido",Toast.LENGTH_SHORT)}
+    fun corazonToggle5(view: View) {Toast.makeText(this,"Añadido",Toast.LENGTH_SHORT)}
+    fun corazonToggle6(view: View) {Toast.makeText(this,"Añadido",Toast.LENGTH_SHORT)}
 }
