2e3d0ebadc6aef4ac6a5129a614f9e6df4b9f257
JoseMBetancourt <josebetancourt@unicauca.edu.co>
2021-04-23 21:12:54 -0500

    Implementacion Cajon de navegacion

--- app/src/main/java/edu/unicauca/rapirecetas/MainActivity.kt
+++ app/src/main/java/edu/unicauca/rapirecetas/MainActivity.kt
@@ -1,16 +1,43 @@
 package edu.unicauca.rapirecetas
 
-import android.content.Intent
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val drawarLayout = findViewById<DrawerLayout>(R.id.drawarLayout)
+        val imgMenu = findViewById<ImageView>(R.id.imgMenu)
+
+        val navView = findViewById<NavigationView>(R.id.navDawar)
+        navView.itemIconTintList = null
+        imgMenu.setOnClickListener {
+            drawarLayout.openDrawer(GravityCompat.START)
+        }
+        val navController = Navigation.findNavController(this,R.id.fragment)
+        NavigationUI.setupWithNavController(navView,navController)
+
+        val textTitle = findViewById<TextView>(R.id.title)
+        navController
+            .addOnDestinationChangedListener { controller, destination, arguments ->
+                textTitle.text = destination.label
+            }
+
+
     }
 
     fun corazonToggle(view: View) {Toast.makeText(this,"Añadido",Toast.LENGTH_SHORT)}
@@ -20,10 +47,6 @@ class MainActivity : AppCompatActivity() {
     fun corazonToggle5(view: View) {Toast.makeText(this,"Añadido",Toast.LENGTH_SHORT)}
     fun corazonToggle6(view: View) {Toast.makeText(this,"Añadido",Toast.LENGTH_SHORT)}
 
-    fun openAccount(view: View) {
-        startActivity(Intent(this, Cuenta::class.java))
-    }
-    fun openMenu(view: View) {
-        startActivity( Intent(this, Categorias::class.java))
-    }
+
+
 }
---
+++ app/src/main/java/edu/unicauca/rapirecetas/view/ProfileFragment.kt
@@ -0,0 +1,20 @@
+package edu.unicauca.rapirecetas.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import edu.unicauca.rapirecetas.R
+
+class ProfileFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+}
